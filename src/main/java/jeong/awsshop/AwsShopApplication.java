4112b4011921c87cf600edf3878735b53487f233
standard-chan <starp321@naver.com>
2026-04-09 17:46:38 +0900

    chore: init

---
+++ src/main/java/jeong/awsshop/AwsShopApplication.java
@@ -0,0 +1,13 @@
+package jeong.awsshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jeong/awsshop/AwsShopApplicationTests.java
@@ -0,0 +1,13 @@
+package jeong.awsshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
