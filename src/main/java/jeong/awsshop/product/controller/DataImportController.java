4ddeede83f4326252899abb0af2d90dc82386771
standard-chan <starp321@naver.com>
2026-04-21 00:20:39 +0900

    feat: Product data import API 추가

---
+++ src/main/java/jeong/awsshop/product/controller/DataImportController.java
@@ -0,0 +1,32 @@
+package jeong.awsshop.product.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.io.InputStream;
+import jeong.awsshop.product.service.dataimport.BulkInsertService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/data-import")
+@AllArgsConstructor
+public class DataImportController {
+
+    private final BulkInsertService bulkInsertService;
+
+    /**
+     * JSONL 파일을 스트림으로 받아서 DB에 batch insert하는 API
+     */
+    @PostMapping("/upload")
+    public String bulkupload(HttpServletRequest request) throws IOException {
+        InputStream inputStream = request.getInputStream();
+
+        bulkInsertService.bulkInsert(inputStream);
+        return "ok";
+    }
+}
