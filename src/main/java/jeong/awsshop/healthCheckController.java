4f2b8a868e7e9fcb45a2bf271e525ce51ce8d54b
standard-chan <starp321@naver.com>
2026-04-21 00:19:20 +0900

    feat: health check API 추가

---
+++ src/main/java/jeong/awsshop/healthCheckController.java
@@ -0,0 +1,15 @@
+package jeong.awsshop;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/health")
+public class healthCheckController {
+
+    @GetMapping()
+    public String healthCheck() {
+        return "ok";
+    }
+}
