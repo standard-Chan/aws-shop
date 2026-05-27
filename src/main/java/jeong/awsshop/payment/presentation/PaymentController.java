5ef4b8e1a00e9b82471019f450cea70e5a3100f6
standard-chan <starp321@naver.com>
2026-05-27 14:43:15 +0900

    feat: payment 중복 생성 429 에러 추가

--- src/main/java/jeong/awsshop/payment/presentation/PaymentController.java
+++ src/main/java/jeong/awsshop/payment/presentation/PaymentController.java
@@ -2,15 +2,19 @@ package jeong.awsshop.payment.presentation;
 
 import jeong.awsshop.payment.application.PaymentService;
 import jeong.awsshop.payment.domain.PaymentRepository;
+import jeong.awsshop.payment.exception.DuplicatePaymentException;
 import jeong.awsshop.payment.infrastructure.tosspayment.dto.TossPaymentConfirmResponse;
 import jeong.awsshop.payment.presentation.dto.ConfirmPaymentRequest;
 import jeong.awsshop.payment.presentation.dto.CreatePaymentRequest;
 import jeong.awsshop.payment.presentation.dto.PaymentResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,7 +24,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class PaymentController {
 
     private final PaymentService paymentService;
-    private final PaymentRepository paymentRepository;
 
     @PostMapping()
     public PaymentResponse createPayment(@RequestBody CreatePaymentRequest request) {
@@ -33,4 +36,9 @@ public class PaymentController {
         return paymentService.confirmPayment(request);
     }
 
+    @ResponseStatus(HttpStatus.CONFLICT)
+    @ExceptionHandler(DuplicatePaymentException.class)
+    public String handleDuplicatePayment(DuplicatePaymentException ex) {
+        return ex.getMessage();
+    }
 }
