a0b034681fdb516c0adc2d1d31fa07362547e0ec
standard-chan <starp321@naver.com>
2026-06-08 20:24:56 +0900

    chore: 사용자 ranking 서버 초기 구축

---
+++ event-pipeline/product-ranking/src/main/java/jeong/awsshop/eventpipeline/productranking/ProductRankingApplication.java
@@ -0,0 +1,12 @@
+package jeong.awsshop.eventpipeline.productranking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductRankingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductRankingApplication.class, args);
+    }
+}
